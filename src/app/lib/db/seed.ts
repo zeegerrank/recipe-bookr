7dc84f596b04affad73df6cb7d4aed7047c1256d
zeegerrank <zeegerrank@gmail.com>
2025-08-23 14:53:09 +0700

    feat: define types for seed documents in the database

--- src/app/lib/db/seed.ts
+++ src/app/lib/db/seed.ts
@@ -25,3 +25,79 @@ async function setIfNotExist(path: string, data: object) {
     ifErrorThrow(error);
   }
 }
+
+// Define type for seed docs
+
+// User - users/{userId}
+type UserSeed = {
+  userId: string;
+  displayName: string;
+  email: string;
+  role: "user" | "editor" | "admin";
+  createdAt: Date;
+};
+
+// Profile DIsplay - profileDisplay/{userId}
+type ProfileDisplaySeed = {
+  userId: string;
+  name: string;
+  avatarURL?: string | null;
+  bio?: string;
+  joinedAt: Date;
+};
+
+// Ingredient - ingredients/{ingredientId}
+type IngredientSeed = {
+  ingredientId: string;
+  name: string;
+  gPerMl: number;
+  defaultUnit:
+    | "g"
+    | "kg"
+    | "ml"
+    | "l"
+    | "tsp"
+    | "tbsp"
+    | "cup"
+    | "fl_oz"
+    | "oz";
+  createdAt: Date;
+};
+
+// Recipe - recipes/{recipeId}
+type RecipeSeed = {
+  recipeId: string;
+  description: string | null;
+  authorId: string;
+  public: boolean;
+  ingredients: RecipeIngretients[];
+  steps: string[];
+  tages: string[];
+  createdAt: Date;
+};
+
+// recipe.ingredients[] (embedded_array)
+type RecipeIngretients = {
+  ingredientId: string;
+  name: string;
+  amount: number;
+  unit: "g" | "kg" | "ml" | "l" | "tsp" | "tbsp" | "cup" | "fl_oz" | "oz";
+  notes: string;
+};
+
+// Comment - subcollection: recipes/{recipeId}/comments/{authorId}
+type CommentSeed = {
+  recipeId: string;
+  authorId: string;
+  text: string;
+  createdAt: Date;
+};
+
+// Like - subcollection: recipes/{recipeId}/likes/{authorId}
+
+type Like = {
+  recipeId: string;
+  authorId: string;
+  liked: boolean;
+  updatedAt?: Date | null;
+};
